fe7c4a0c6f9afc3cceb5a810a54e614dfaf8b295
W.M.M.T. Wijerathna <maheshtharanga49@gmail.com>
2024-01-06 17:59:26 +0530

    dsfsdfsdf

---
+++ snap2/snap2/lib/config.dart
@@ -0,0 +1,7 @@
+final url = 'http://192.168.8.118:3000/';
+
+final registration = url + "register";
+final login = url + 'login';
+final addtodo = url + 'createToDo';
+final getToDoList = url + 'getUserTodoList';
+final deleteTodo = url + 'deleteTodo';
